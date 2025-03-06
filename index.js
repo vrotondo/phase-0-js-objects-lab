148bfbfdc709022d6f1664d25dc17813a8ee8b05
vrotondo <150374510+vrotondo@users.noreply.github.com>
2025-03-06 11:46:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150
+}
 
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
 
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
